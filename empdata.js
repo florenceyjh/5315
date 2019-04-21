70e0895d365b2651f99e8a9a78f7d56ac7b85dff
florence <yanj2@seattleu.edu>
2019-04-20 18:28:04 -0700

    5315

--- empdata.js
+++ empdata.js
@@ -1,124 +1,109 @@
 {
   "data": [
     {
-      "name": "James Bond",
-      "designation": "Accountant",
-      "salary": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
-      "extension": "8422"
+      "Title": "Freddie Mercury Album",
+      "Artist": "Freddie Mercury",
+      "Country": "USA",
+      "Company": "EMI",
+      "Price": "$10.9"
     },
     {
-      "name": "Brielle Williamson",
-      "designation": "Integration Specialist",
-      "salary": "$372,000",
-      "joining_date": "2012/12/02",
-      "office": "New York",
-      "extension": "4804"
+      "Title": "Volumn I",
+      "Artist": "Billy Joel",
+      "Country": "UK",
+      "Company": "CBS",
+      "Price": "$9.9"
     },
     {
-      "name": "Ashton Cox",
-      "designation": "Junior Technical Author",
-      "salary": "$86,000",
-      "joining_date": "2009/01/12",
-      "office": "San Francisco",
-      "extension": "1562"
+      "Title": "James Bond",
+      "Artist": "Accountant",
+      "Country": "$170,750",
+      "Company": "2011/07/25",
+      "Price": "Tokyo"
     },
     {
-      "name": "Airi Satou",
-      "designation": "Accountant",
-      "salary": "$162,700",
-      "joining_date": "2008/11/28",
-      "office": "Tokyo",
-      "extension": "5407"
+      "Title": "James Bond",
+      "Artist": "Accountant",
+      "Country": "$170,750",
+      "Company": "2011/07/25",
+      "Price": "Tokyo"
     },
     {
-      "name": "Caesar Vance",
-      "designation": "Pre-Sales Support",
-      "salary": "$106,450",
-      "joining_date": "2011/12/12",
-      "office": "New York",
-      "extension": "8330"
+      "Title": "James Bond",
+      "Artist": "Accountant",
+      "Country": "$170,750",
+      "Company": "2011/07/25",
+      "Price": "Tokyo"
     },
     {
-      "name": "Shad Decker",
-      "designation": "Regional Director",
-      "salary": "$183,000",
-      "joining_date": "2008/11/13",
-      "office": "Edinburgh",
-      "extension": "6373"
+      "Title": "James Bond",
+      "Artist": "Accountant",
+      "Country": "$170,750",
+      "Company": "2011/07/25",
+      "Price": "Tokyo"
     },
     {
-      "name": "Cedric Kelly",
-      "designation": "Senior Javascript Developer",
-      "salary": "$433,060",
-      "joining_date": "2012/03/29",
-      "office": "Edinburgh",
-      "extension": "6224"
+      "Title": "James Bond",
+      "Artist": "Accountant",
+      "Country": "$170,750",
+      "Company": "2011/07/25",
+      "Price": "Tokyo"
     },
     {
-      "name": "Haley Kennedy",
-      "designation": "Senior Marketing Designer",
-      "salary": "$313,500",
-      "joining_date": "2012/12/18",
-      "office": "London",
-      "extension": "3597"
+      "Title": "James Bond",
+      "Artist": "Accountant",
+      "Country": "$170,750",
+      "Company": "2011/07/25",
+      "Price": "Tokyo"
     },
     {
-      "name": "Colleen Hurst",
-      "designation": "Javascript Developer",
-      "salary": "$205,500",
-      "joining_date": "2009/09/15",
-      "office": "San Francisco",
-      "extension": "2360"
+      "Title": "James Bond",
+      "Artist": "Accountant",
+      "Country": "$170,750",
+      "Company": "2011/07/25",
+      "Price": "Tokyo"
     },
     {
-      "name": "Dai Rios",
-      "designation": "Personnel Lead",
-      "salary": "$217,500",
-      "joining_date": "2012/09/26",
-      "office": "Edinburgh",
-      "extension": "2290"
+      "Title": "James Bond",
+      "Artist": "Accountant",
+      "Country": "$170,750",
+      "Company": "2011/07/25",
+      "Price": "Tokyo"
     },
     {
-      "name": "Herrod Chandler",
-      "designation": "Sales Assistant",
-      "salary": "$137,500",
-      "joining_date": "2012/08/06",
-      "office": "San Francisco",
-      "extension": "9608"
+      "Title": "James Bond",
+      "Artist": "Accountant",
+      "Country": "$170,750",
+      "Company": "2011/07/25",
+      "Price": "Tokyo"
     },
     {
-      "name": "Rhona Davidson",
-      "designation": "Integration Specialist",
-      "salary": "$327,900",
-      "joining_date": "2010/10/14",
-      "office": "Tokyo",
-      "extension": "6200"
+      "Title": "James Bond",
+      "Artist": "Accountant",
+      "Country": "$170,750",
+      "Company": "2011/07/25",
+      "Price": "Tokyo"
     },
     {
-      "name": "Sonya Frost",
-      "designation": "Software Engineer",
-      "salary": "$103,600",
-      "joining_date": "2008/12/13",
-      "office": "Edinburgh",
-      "extension": "1667"
+      "Title": "James Bond",
+      "Artist": "Accountant",
+      "Country": "$170,750",
+      "Company": "2011/07/25",
+      "Price": "Tokyo"
     },
     {
-      "name": "Jena Gaines",
-      "designation": "Office Manager",
-      "salary": "$90,560",
-      "joining_date": "2008/12/19",
-      "office": "London",
-      "extension": "3814"
+      "Title": "James Bond",
+      "Artist": "Accountant",
+      "Country": "$170,750",
+      "Company": "2011/07/25",
+      "Price": "Tokyo"
     },
     {
-      "name": "Quinn Flynn",
-      "designation": "Support Lead",
-      "salary": "$342,000",
-      "joining_date": "2013/03/03",
-      "office": "Edinburgh",
-      "extension": "9497"
+      "Title": "James Bond",
+      "Artist": "Accountant",
+      "Country": "$170,750",
+      "Company": "2011/07/25",
+      "Price": "Tokyo"
     }
   ]
 }
